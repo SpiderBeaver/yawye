b4b7c2ef88deb9b67d020182d9c98d6b9b689308
SpiderBeaver <spdrbeaver@gmail.com>
2021-03-28 15:56:55 +0300

    Remove unused counter component.

--- yawye-frontend/src/app/store.ts
+++ yawye-frontend/src/app/store.ts
@@ -1,12 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import dietReducer from '../features/diet/dietSlice';
 import dishesReducer from '../features/dishes/dishesSlice';
 import ingredientsReducer from '../features/ingredients/ingredientsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     diet: dietReducer,
     dishes: dishesReducer,
     ingredients: ingredientsReducer,
